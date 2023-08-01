559a7a5b11b801249ed868b93b65b5c4972c76b3
Adam <henault.adam@gmail.com>
2023-08-01 02:30:58 +0200

    Fix DLL Unload

--- Source/D3D9 ImGui Hook/dllmain.cpp
+++ Source/D3D9 ImGui Hook/dllmain.cpp
@@ -1,40 +1,22 @@
 #include "pch.h"
 #include "Hook.h"
 
-HANDLE hThread;
-
-int Initialize(const HMODULE hModule)
+void Initialize()
 {
 	Hook::HookDirectX();
-
-	while(!GetAsyncKeyState(VK_END))
-	{
-		Sleep(1);
-	}
-
-	Hook::UnHookDirectX();
-
-	FreeLibraryAndExitThread(hModule, 0);
 }
 
 BOOL WINAPI DllMain(const HINSTANCE hinstDLL, const DWORD fdwReason, LPVOID lpReserved)
 {
 	if (fdwReason == DLL_PROCESS_ATTACH)
 	{
-		hThread = CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)Initialize, hinstDLL, 0, nullptr);
-		if (hThread != INVALID_HANDLE_VALUE && hThread != nullptr)
-			CloseHandle(hThread);
+		CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)Initialize, nullptr, 0, nullptr);
 	}
 
 	if (fdwReason == DLL_PROCESS_DETACH)
 	{
 		Hook::UnHookDirectX();
-		if (hThread != INVALID_HANDLE_VALUE && hThread != nullptr)
-		{
-			TerminateThread(hThread, 0);
-			CloseHandle(hThread);
-		}
-		FreeLibraryAndExitThread(hinstDLL, 0);
+		FreeLibrary(hinstDLL);
 	}
 
 	return TRUE;
